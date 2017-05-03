79539d06c25897bdc38730713dc66b199040b94c
moevegas <moevegas@gmail.com>
2017-05-02 20:57:52 -0400

    Add project files.

---
+++ BlueBuzz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueBuzz.MvcApplication" Language="C#" %>
---
+++ BlueBuzz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueBuzz.Startup))]
+namespace BlueBuzz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
